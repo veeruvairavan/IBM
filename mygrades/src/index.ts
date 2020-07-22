ee148950bdf2caa8b51c69807bc23da48a6129e6
Veerappan Vairavan <veerappanvairavan@Veerappans-MacBook-Pro.local>
2020-07-22 08:47:19 -0400

    cors credentials true

--- mygrades/src/index.ts
+++ mygrades/src/index.ts
@@ -35,7 +35,8 @@ if (require.main === module) {
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
         preflightContinue: false,
         optionsSuccessStatus: 204,
-        maxAge: 86400
+        maxAge: 86400,
+        credentials : true
       }
     },
   };
